fc6eef6526601c16d8b76b05b34d698e089edb8b
biast12 <tobiaspoulsen@hotmail.com>
2025-12-14 23:34:59 +0100

    RM-26: Edit the panel and multipanel messages on edit
    This PR allows the panel and multipanel messages to be edited when they're being edited instead of resending them, which fallback on resending the message in case it can't edit it (if the message is deleted for an example)

--- app/http/endpoints/api/panel/multipanelmessagedata.go
+++ app/http/endpoints/api/panel/multipanelmessagedata.go
@@ -123,3 +123,84 @@ func (d *multiPanelMessageData) send(ctx *botcontext.BotContext, panels []databa
 
 	return msg.Id, nil
 }
+
+func (d *multiPanelMessageData) edit(ctx *botcontext.BotContext, messageId uint64, panels []database.Panel) error {
+	if !d.IsPremium {
+		d.Embed.SetFooter(fmt.Sprintf("Powered by %s", config.Conf.Bot.PoweredBy), config.Conf.Bot.IconUrl)
+	}
+
+	var components []component.Component
+	if d.SelectMenu {
+		options := make([]component.SelectOption, len(panels))
+		for i, panel := range panels {
+			emoji := types.NewEmoji(panel.EmojiName, panel.EmojiId).IntoGdl()
+
+			options[i] = component.SelectOption{
+				Label: panel.ButtonLabel,
+				Value: panel.CustomId,
+				Emoji: emoji,
+			}
+		}
+
+		var placeholder string
+		if d.SelectMenuPlaceholder == nil {
+			placeholder = "Select a topic..."
+		} else {
+			placeholder = *d.SelectMenuPlaceholder
+		}
+
+		components = []component.Component{
+			component.BuildActionRow(
+				component.BuildSelectMenu(
+					component.SelectMenu{
+						CustomId:    "multipanel",
+						Options:     options,
+						Placeholder: placeholder,
+						MinValues:   utils.IntPtr(1),
+						MaxValues:   utils.IntPtr(1),
+						Disabled:    false,
+					}),
+			),
+		}
+	} else {
+		buttons := make([]component.Component, len(panels))
+		for i, panel := range panels {
+			emoji := types.NewEmoji(panel.EmojiName, panel.EmojiId).IntoGdl()
+
+			buttons[i] = component.BuildButton(component.Button{
+				Label:    panel.ButtonLabel,
+				CustomId: panel.CustomId,
+				Style:    component.ButtonStyle(panel.ButtonStyle),
+				Emoji:    emoji,
+				Disabled: panel.Disabled,
+			})
+		}
+
+		var rows []component.Component
+		for i := 0; i <= int(math.Ceil(float64(len(buttons)/5))); i++ {
+			lb := i * 5
+			ub := lb + 5
+
+			if ub >= len(buttons) {
+				ub = len(buttons)
+			}
+
+			if lb >= ub {
+				break
+			}
+
+			row := component.BuildActionRow(buttons[lb:ub]...)
+			rows = append(rows, row)
+		}
+
+		components = rows
+	}
+
+	data := rest.EditMessageData{
+		Embeds:     []*embed.Embed{d.Embed},
+		Components: components,
+	}
+
+	_, err := rest.EditMessage(context.Background(), ctx.Token, ctx.RateLimiter, d.ChannelId, messageId, data)
+	return err
+}
